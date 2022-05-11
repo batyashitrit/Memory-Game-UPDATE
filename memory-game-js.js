dd7c55b20830ed842773699f059ff61dedb5fc01
batya <batya.yel@gmail.com>
2022-05-11 12:35:49 +0300

    update with chaging players and num cards

--- memory-game-js.js
+++ memory-game-js.js
@@ -1,91 +1,163 @@
 // let cards = ["🌹", "🌺", "🌻", "🌼", "🏵️", "💮"];
-let cards = []
+let cards = [];
 let shuffled = [];
-let players = []
+let players = [];
 let board = document.createElement("div");
-board.id = "game-table"
+board.id = "game-table";
 
-let cardNumReasult ;
+let cardNumReasult;
 
-function craetPlayer(playerNum, playerName,score) { // יוצרת את השחקן
+function craetPlayer(playerNum, playerName, score) {
+  // יוצרת את השחקן
   return {
     playerNum,
     playerName,
     score,
-  }
+  };
+}
+let $btn;
+function numberOfCards() {
+  // יוצרת מקום לכתיבת מספר הזוגות, טקסט וכפתור
+  let cardNum = document.createElement("input");
+  let message = document.createElement("span");
+  let $btn = document.createElement("button");
+  message.innerText = `How many pairs you want (max-30)?`;
+  message.id = "message";
+  cardNum.type = "number";
+  cardNum.id = "numcheck";
+  cardNum.max = 30;
+  cardNum.min = 1;
+  $btn.innerText = "Click to start";
+  $btn.id = "btn";
+  document.body.appendChild(cardNum);
+  document.body.appendChild(message);
+  document.body.appendChild($btn);
+
+  deleteNumberOfCards();
 }
 
-function numberOfCards(){ // יוצרת מקום לכתיבת מספר הזוגות, טקסט וכפתור
-let cardNum = document.createElement("input")
-let message = document.createElement("span")
-let $btn = document.createElement("button")
-message.innerText = `       Welcome to the memory-game!
-       How many pairs you want (max-26)?`
-message.id = "message"
-cardNum.type = "number"  
-cardNum.id ="numcheck"
-cardNum.max = 26
-cardNum.min = 1
-$btn.innerText = "click"
-$btn.id = "btn"
-document.body.appendChild(cardNum);
-document.body.appendChild(message);
-document.body.appendChild($btn);
+// numberOfCards();
 
-deleteNumberOfPlayers()
-}
-function format(input){
-  if(input.value.length === 1){
-    input.value = "0" + input.value;
-  }
-}
-numberOfCards()
+function deleteNumberOfCards() {
+  // שומרת את מספר הכרטיסים במשתנה ומוחקת את האלמנטים הנל
+  let cardNum = document.getElementById("numcheck");
+  let message = document.getElementById("message");
+  let $btn = document.getElementById("btn");
+ 
+  cardNum.addEventListener("input", function (event) {
+    if (event.target.valueAsNumber < 1 || event.target.valueAsNumber > 30) {
+      // לודא שלא יוכנס מספר שלא בטווח
+      document.getElementById(
+        "message"
+      ).innerHTML = `Error. Please select a number from 1 to 30`;
+      document.body.removeChild($btn);
+    } else {
+      document.getElementById(
+        "message"
+      ).innerHTML = ``;
+      document.body.appendChild($btn);
 
-function deleteNumberOfPlayers(){ // שומרת את מספר הכרטיסים במשתנה ומוחקת את האלמנטים הנל
-  let cardNum = document.getElementById("numcheck")
-  let message = document.getElementById("message")
-  let $btn = document.getElementById("btn")
-  cardNum.addEventListener("input",function(event){
-    cardNumReasult =  event.target.valueAsNumber
-    })
-  $btn.addEventListener("click",function(event){
-    document.body.removeChild(cardNum)
+      cardNumReasult = event.target.valueAsNumber;
+    }
+  });
+  $btn.addEventListener("click", function (event) {
+    document.body.removeChild(cardNum);
     document.body.removeChild(message);
     document.body.appendChild(board);
-    addCards()
+    addCards();
+  });
+}
+
+
+let numberPlayers;
+function addplayers(){
+  let $numPlayer= document.createElement("input")
+  let messagePlayers = document.createElement("span");
+  $btnPlayers = document.createElement("button");
+  $btnPlayers.innerText = "Click to continue";
+  $btnPlayers.id = "btn-players";
+  $numPlayer.type = "number"
+  $numPlayer.id = "num-players"
+  messagePlayers.innerText =`How many players want to play?`
+  messagePlayers.id = "message-Players"
+  document.body.appendChild($numPlayer);
+document.body.appendChild(messagePlayers);
+document.body.appendChild($btnPlayers);
+  $numPlayer.addEventListener("input",function(e){
+    numberPlayers = e.target.valueAsNumber
+    for(let i = 0; i< numberPlayers; i++){
+      addNamesPlayers()
+    }
+  })
+  deleteNumberOfPlayers()
+}
+addplayers()
+
+function addCards() {
+  // יוצרת מערך של כרטיסים בהתאם למה שהמשתמש בחר ומתחילה לבנות את כל המשחק
+  let emoje = ["🍄", "🍅", "🍆", "🍇", "🍈", "🍉", "🍊" ,"🍋" , "🍌", "🍍", "🍎", "🍏", "🍐", "🍑", "🍒", "🍓", "🍔", "🍕", "🍖", "🍗", "🍘", "🍙", "🍚", "🍛", "🍜", "🍝", "🍞", "🍟", "🍠", "🍡"]
+  for (let i = 0; i < cardNumReasult; i++) {
+    cards.push(emoje[i]);
+  }
+  let cards2 = cards.concat(cards);
+  shuffle(cards2);
+  addPlayersToBoard();
+  addchild(shuffled);
+}
+// let numberPlayers = prompt(`Welcome to the memory-game!
+// How many pepole want to play?`);
+let i = -1;
+for (let i = 0; i < numberPlayers; i++) {
+  addNamesPlayers();
+} 
+ let playerNum;
+let playerNameText
+
+function addNamesPlayers() {
+  // הוספת שחקנים למשחק ובחירת שמות
+  let $playerName = document.createElement("input")
+    // let $btn = document.getElementById("btn");
+  $playerName.type = "text"
+  $playerName.className = "playersNames"
+  $playerName.placeholder = "enter your name"
+  document.body.appendChild($playerName);
+  $btnPlayers.addEventListener("click",function(){
+    playerNameText = $playerName.value
+    console.log($playerName.value)
+    for (i; i < numberPlayers; i++) {
+      i++;
+      playerNum = i;
+      break;
+    }
+    addNewplayer()
   })
 }
+// debugger
+function deleteNumberOfPlayers() {
+  let $numPlayer = document.getElementById("num-players");
+  let messagePlayers = document.getElementById("message-Players");
+  let $btn = document.getElementById("btn");
+  $btnPlayers.addEventListener("click", function () {
+    $numPlayer.parentNode.removeChild($numPlayer);
+    messagePlayers.parentNode.removeChild(messagePlayers);
+    document.body.removeChild($btnPlayers);
+  removeElementsByClass("playersNames")  
+  numberOfCards();
 
-  function addCards(){ // יוצרת מערך של כרטיסים בהתאם למה שהמשתמש בחר ומתחילה לבנות את כל המשחק
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    for(let i=0; i<cardNumReasult; i++){
-      cards.push(alphabet[i])
-    }
-    let cards2 = cards.concat(cards);
-    shuffle(cards2);
-    addPlayersToBoard();
-    addchild(shuffled);
-    
+  });
 }
-let numberPlayers = (prompt(`Welcome to the memory-game!
-How many pepole want to play?`))
-let i = -1
-function addNewplayer() {
-  let playerNum;
-  for(i ; i< numberPlayers; i++){
-    i++
-    playerNum = i
-    break
+
+function removeElementsByClass(className){ // הסרה של תיבות שמות המשתתפים
+  const elements = document.getElementsByClassName(className);
+  while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
   }
-  let Playername = prompt(`please enter your name: `)
-  let score = 0
-  let newPlayer = craetPlayer(playerNum, Playername, score);
+}
+function addNewplayer(){
+  let score = 0;
+  let newPlayer = craetPlayer(playerNum, playerNameText, score);
   players.push(newPlayer);
   return newPlayer;
-}
-
-for(let i = 0; i< numberPlayers; i++){
-  addNewplayer()
 }
 
 function addPlayersToBoard() {
@@ -98,6 +170,7 @@ function addPlayersToBoard() {
     document.body.appendChild(p);
     let elem = document.getElementById(players[0].playerNum);
     elem.className = "turn";
+    currentPlayer = players[0];
   });
 }
 
@@ -109,10 +182,10 @@ function shuffle(arr) {
     i = 0;
   }
 }
-
-let currentPlayer = players[0];
+let currentPlayer;
 let card1 = [];
 let cardDone = 0;
+// הפונקציה של המשחק שבודקת כל זוג אם הוא שווה
 function flipcard(event) {
   if (card1.length <= 2) {
     event.target.classList.add("show-card");
@@ -143,6 +216,7 @@ function flipcard(event) {
 }
 
 function addchild(arr) {
+  // מכניס כרטיסים ללוח
   for (x of arr) {
     let elem = document.createElement("div"); // הגדרה של הכרטיס שלנו שהוא אובייקט
     elem.innerText = x;
@@ -156,21 +230,21 @@ function removeCard() {
   card1.forEach((v) => v.classList.add("remove-all-styles"));
   card1 = [];
 }
-let c = 0
+let c = 0;
 function changeplayer() {
   changeTurn();
-  for(c++; c<numberPlayers;){
+  for (c++; c < numberPlayers; ) {
     currentPlayer = players[c];
     let elem = document.getElementById(players[c].playerNum);
     elem.className = "turn";
-    break
+    break;
   }
-  if(c >= numberPlayers) {
+  if (c >= numberPlayers) {
     changeTurn();
-    c=0
-      currentPlayer = players[c];
-      let elem = document.getElementById(players[c].playerNum);
-      elem.className = "turn";
+    c = 0;
+    currentPlayer = players[c];
+    let elem = document.getElementById(players[c].playerNum);
+    elem.className = "turn";
   }
 }
 
@@ -184,25 +258,22 @@ function winner() {
   let totalScore = 0;
   players.filter((v) => (totalScore += v.score));
   if (totalScore == cardNumReasult) {
-    changeTurn(); 
+    changeTurn();
     let winnerText = document.createElement("div");
     winnerText.className = "winnerText";
-    let playersScore = players.map(v => v.score)
+    let playersScore = players.map((v) => v.score);
     let bastScore = Math.max.apply(null, playersScore);
-    let winners = players.filter(v=> v.score == bastScore)
-    let winnersName = winners.map(v=> v.playerName)
-    if(winnersName.length < 2){
-        winnerText.innerText = `The winner is ${winnersName}`;
-        board.appendChild(winnerText)
-        // setTimeout(startAgain,1000)
-        
-    }
-    else{
+    let winners = players.filter((v) => v.score == bastScore);
+    let winnersName = winners.map((v) => v.playerName);
+    if (winnersName.length < 2) {
+      winnerText.innerText = `The winner is ${winnersName}`;
+      board.appendChild(winnerText);
+      // setTimeout(startAgain,1000)
+    } else {
       winnerText.innerText = `The winners are ${winnersName}`;
-      board.appendChild(winnerText)
+      board.appendChild(winnerText);
     }
   }
-
 }
 
 // function startAgain(){
@@ -213,6 +284,6 @@ function winner() {
 //   addchild(shuffled);
 // }
 
-    // shuffle(cards2);
-    // addPlayersToBoard();
-    // addchild(shuffled);
+// shuffle(cards2);
+// addPlayersToBoard();
+// addchild(shuffled);
